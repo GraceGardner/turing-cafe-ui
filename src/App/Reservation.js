675077e692e2f546f4d7d71537806f5eca5040d5
Grace Gardner <graceirisgardner@gmail.com>
2022-02-16 09:35:32 -0700

    create jsx for reservation functional component

--- src/App/Reservation.js
+++ src/App/Reservation.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './Reservation.css';
 
-const Reservation = () => {
-  
+const Reservation = ({id, name, data, time, number, cancelReservation}) => {
+  return (
+    <div className='reservation'>
+      <h2 className='name'>{name}</h2>
+      <p className='date'>{date}</p>
+      <p className='time'>{`${time} pm`}</p>
+      <p className='number'>{`Number of guests : ${number}`}</p>
+      <button className='cancel-button' onClick={() => cancelReservation(id)}>Cancel</button>
+    </div>
+  )
 }
 
 export default Reservation;
