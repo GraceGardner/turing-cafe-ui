987bb8c4fe83abbe7e7529a5c6de4c2614da266c
Grace Gardner <graceirisgardner@gmail.com>
2022-02-16 10:42:13 -0700

    add boiler plate for form

--- src/App/Form.js
+++ src/App/Form.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Form.css';
+
+class Form extends Component {
+  constructor(){
+    super()
+    this.state = {
+      name: '',
+      date: undefined,
+      time: undefined,
+      number: undefined
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
+  // reserve = (event) => {
+  //   event.preventDefault()
+  //
+  // }
+
+  // reset = () => {
+  //
+  // }
+
+  
+}
 
 export default Form;
 
