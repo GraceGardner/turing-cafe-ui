24d30c2fca83dca0ed8934d733c8bfff1aaff4f8
Grace Gardner <graceirisgardner@gmail.com>
2022-02-16 09:27:16 -0700

    basic setup

---
+++ src/App/Form.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import './Form.css';
+
+export default Form;
---
+++ src/App/Reservation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Reservation.css';
+
+const Reservation = () => {
+  
+}
+
+export default Reservation;
+
+// {
+// "id": 1,
+// "name": "Christie",
+// "date": "12/29",
+// "time": "7:00",
+// "number": 12
+// },
