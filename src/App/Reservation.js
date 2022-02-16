d207cf39b3d478516d2daba26e8484a549bb0aa8
Grace Gardner <graceirisgardner@gmail.com>
2022-02-16 09:58:28 -0700

    make card to map

--- src/App/Reservation.js
+++ src/App/Reservation.js
@@ -3,16 +3,26 @@ import Card from './Card.js'
 import './Reservation.css';
 
 const Reservation = ({reservations, cancelReservation}) => {
-  return (
-    <Card
+
+  const allReservations = reservations.map(reservation => {
+    return (
+      <Card
       id={reservation.id}
       name={reservation.name}
       date={reservation.date}
       time={reservation.time}
       number={reservation.number}
       cancelReservation={cancelReservation}
-    />
+      />
+    )
+  })
+
+  return (
+    <div className='res-container'>
+      {allReservations}
+    </div>
   )
+
 }
 
 export default Reservation;
